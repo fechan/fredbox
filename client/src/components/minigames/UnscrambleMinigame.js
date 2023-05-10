a671530f10cf255fd0f28e1a9bb198927e05a41a
fechan <56131910+fechan@users.noreply.github.com>
2023-05-09 23:01:24 -0700

    add animation to scramble

--- client/src/components/minigames/UnscrambleMinigame.js
+++ client/src/components/minigames/UnscrambleMinigame.js
@@ -4,14 +4,22 @@ import { socket } from "../../socket";
 export function UnscrambleMinigame({ minigameID, choices }) {
   const [ intermedAnswer, setIntermedAnswer ] = useState("");
   const [ remainingChoices, setRemainingChoices ] = useState([]);
+  const [ date, setDate ] = useState(Date.now());
 
   useEffect(() => {
     setRemainingChoices(choices);
     setIntermedAnswer("");
   }, [choices]);
+  
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setDate(Date.now());
+    }, 1000 / 144);
 
-  function addToAnswer(evt) {
-    const letter = evt.target.value;
+    return () => clearInterval(intervalId);
+  }, [date]);
+
+  function addToAnswer(letter, index) {
     if (intermedAnswer.length + 1 === 4) {
       setAnswer(intermedAnswer + letter);
     }
@@ -19,9 +27,13 @@ export function UnscrambleMinigame({ minigameID, choices }) {
 
     // remove the selected choice from remaining choices
     let newRemainingChoices = [...remainingChoices];
-    const indexOfChoice = newRemainingChoices.findIndex(choice => choice === letter);
-    newRemainingChoices.splice(indexOfChoice, 1);
+    newRemainingChoices[index] = "";
     setRemainingChoices(newRemainingChoices);
+  }
+
+  function resetAnswer() {
+    setIntermedAnswer("");
+    setRemainingChoices(choices);
   }
 
   function setAnswer(answer) {
@@ -30,16 +42,46 @@ export function UnscrambleMinigame({ minigameID, choices }) {
     });
   }
 
+  const ms_per_min = 60000;
+  const RPM = 5;
+  const ms_per_rot = ms_per_min / RPM;
+  const globalAngle = (date / ms_per_rot) * 2 * Math.PI;
+
+  let intermedAnswerDisplay = "";
+  for (let i=0; i < 4; i++) {
+    if (i < intermedAnswer.length) {
+      intermedAnswerDisplay += intermedAnswer.charAt(i) + " ";
+    } else {
+      intermedAnswerDisplay += "_ ";
+    }
+  }
+  
   return (
     <div className="UnscrambleMinigame">
+      <h2>Unscramble the word!</h2>
       <div className="choice-container">
         {
-          remainingChoices.map((letter, i) => 
-            <button key={ String(minigameID) + i } onClick={ addToAnswer } value={ letter }>{ letter }</button>
-          )
+          remainingChoices.map((letter, i) => {
+            const angle = globalAngle + (i * Math.PI / 2);
+            const y = Math.sin(angle);
+            const x = Math.cos(angle);
+            const style = {
+              transform: `translateX(calc(${x} * 250px / 2)) translateY(calc(${y} * 250px / 2))`
+            }
+      
+            return (
+              <button style={ style }
+                  className="choice"
+                  key={ String(minigameID) + i }
+                  onClick={ () => addToAnswer(letter, i) }>
+                { letter }
+              </button>
+            )
+          })
         }
       </div>
-      <div>{ intermedAnswer }</div>
+      <div className="intermediate-answer">{ intermedAnswerDisplay }</div>
+      <button onClick={ resetAnswer } className="btn btn-outline-danger">Reset</button>
     </div>
   );
 }
