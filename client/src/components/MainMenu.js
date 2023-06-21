c86a59ab539dd90371ef6cb8fc7774d6f396d753
fechan <56131910+fechan@users.noreply.github.com>
2023-06-20 19:07:40 -0700

    add tagline

--- client/src/components/MainMenu.js
+++ client/src/components/MainMenu.js
@@ -2,6 +2,7 @@ export function MainMenu({onJoinRoomClicked, onCreateRoomClicked}) {
   return (
     <div>
       <h2 className="fw-bold">Main menu</h2>
+      <p className="lead fw-bold">This is a game where you compete against your friends in simple brain games</p>
       <button className="btn btn-primary" onClick={ onJoinRoomClicked }>Join room</button>
       <button className="btn btn-outline-primary" onClick={ onCreateRoomClicked }>Create room</button>
     </div>
