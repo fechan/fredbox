082ebb14ceaf217d1d90891f51cb9135eb58661d
fechan <56131910+fechan@users.noreply.github.com>
2024-03-22 22:35:07 -0700

    document behavior when undefined

--- client/src/socket.js
+++ client/src/socket.js
@@ -1,5 +1,7 @@
 import { io } from "socket.io-client";
 
+// setting URL to undefined causes socket.io to autodetect the WebSocket server
+// endpoint based on the URL it's being served from
 const URL = process.env.NODE_ENV === "production" ? undefined : "http://localhost:5000";
 
 export const socket = io(URL);
